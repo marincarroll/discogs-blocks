fb9ca675c508c0e938e2709d96b8ba2ed29d2760
Carroll <m.carroll@northeastern.edu>
2025-10-30 10:34:20 -0400

    logic for notices

--- inc/class-options-page.php
+++ inc/class-options-page.php
@@ -2,14 +2,17 @@
 /**
  * Creates options page to store API authentication.
  */
+
 // TODO add uninstaller to delete options from database
 namespace Marincarroll\Discogs;
 
 class OptionsPage {
-	private $_options;
+	private $_access_token;
 	private $_page_slug = 'marincarroll_discogs';
 
 	public function __construct() {
+		$this->_access_token = get_option( 'access_token' );
+
 		add_action( 'admin_menu', array( $this, 'create_options_page' ) );
 		add_action( 'admin_menu', array( $this, 'add_authentication_settings_section' ) );
 		add_action( 'admin_init', array( $this, 'register_authentication_settings' ) );
@@ -17,17 +20,17 @@ class OptionsPage {
 
 	public function create_options_page() {
 		add_options_page(
-			__('Discogs Blocks Settings', 'discogs-blocks'),
-			__('Discogs Blocks', 'discogs-blocks'),
+			__( 'Discogs Blocks Settings', 'discogs-blocks' ),
+			__( 'Discogs Blocks', 'discogs-blocks' ),
 			'manage_options',
 			'marincarroll_discogs',
-			function() {
+			function () {
 				?>
 				<div class="wrap">
 					<form method="post" action="options.php">
 						<h1><?php esc_html_e( get_admin_page_title() ) ?></h1>
 						<?php settings_fields( $this->_page_slug ) ?>
-						<?php do_settings_sections($this->_page_slug ) ?>
+						<?php do_settings_sections( $this->_page_slug ) ?>
 						<?php submit_button() ?>
 					</form>
 				</div>
@@ -36,12 +39,30 @@ class OptionsPage {
 		);
 	}
 
+	public function render_authentication_notice() {
+		if( ! $this->_access_token ) {
+			$message = sprintf(
+				__('%s <a href="%s" target="_blank">%s</a>.', 'discogs-blocks'),
+				__( 'You can generate a new access token by visiting', 'discogs-blocks' ),
+				esc_url( 'https://www.discogs.com/settings/developers'),
+				__( 'Discogs Developer Settings', 'discogs-blocks' )
+			);
+
+			return '<div class="notice inline"><p>' . $message . '</p></div>';
+		} else {
+			return '<div class="notice inline notice-info"><p>this will be an identity panel</p></div>';
+		}
+	}
+
 	public function add_authentication_settings_section() {
 		add_settings_section(
 			$this->_page_slug . '_authentication',
-			__('API authentication', 'discogs-blocks'),
+			__( 'API authentication', 'discogs-blocks' ),
 			'',
-			$this->_page_slug
+			$this->_page_slug,
+			array(
+				'after_section' => $this->render_authentication_notice(),
+			)
 		);
 
 		$token = get_option( $this->_page_slug . '_authentication_token' );
@@ -49,21 +70,18 @@ class OptionsPage {
 		add_settings_field(
 			'access_token',
 			'Personal Access Token',
-			array( $this, 'authentication_token_field_callback' ),
+			array( $this, 'render_token_field' ),
 			$this->_page_slug,
 			$this->_page_slug . '_authentication'
 		);
 	}
 
-	public function authentication_token_field_callback() {
-		$option_name = 'access_token';
-		$option = get_option( $option_name );
-
+	public function render_token_field() {
 		printf(
 			'<label for="%1$s" class="screen-reader-text">%2$s</label><input type="text" id="%1$s" name="%1$s" value="%3$s" />',
-			$option_name,
-			__('Enter Personal Access Token', 'discogs-blocks'),
-			isset( $option ) ? esc_attr( $option ) : '',
+			'access_token',
+			__( 'Enter Personal Access Token', 'discogs-blocks' ),
+			isset( $this->_access_token ) ? esc_attr( $this->_access_token ) : '',
 		);
 	}
 
@@ -71,10 +89,21 @@ class OptionsPage {
 		register_setting(
 			$this->_page_slug,
 			'access_token',
-			array (
-				'type'    => 'string',
-				'default' => ''
+			array(
+				'type' => 'string',
+				'sanitize_callback' => array( $this, 'sanitize_token_field' ),
+				//'show_in_rest' => false,
+				//'label' => '',
+				//'description' => '',
 			)
+		);
+	}
+
+	public function sanitize_token_field( $input ) {
+		return preg_replace(
+			'/[^A-Za-z]/',
+			'',
+			$input
 		);
 	}
 }
