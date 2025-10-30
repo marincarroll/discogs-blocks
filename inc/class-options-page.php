29daa857528cb55982877f57d4856296517f75d2
Carroll <m.carroll@northeastern.edu>
2025-10-30 13:16:08 -0400

    finish logic for whether PAT is valid

--- inc/class-options-page.php
+++ inc/class-options-page.php
@@ -8,14 +8,34 @@ namespace Marincarroll\Discogs;
 
 class OptionsPage {
 	private $_access_token;
+	private $_discogs_user;
 	private $_page_slug = 'marincarroll_discogs';
 
 	public function __construct() {
-		$this->_access_token = get_option( 'access_token' );
+		$this->_access_token  = get_option( 'access_token' );
+		$this->validate_identity();
 
 		add_action( 'admin_menu', array( $this, 'create_options_page' ) );
 		add_action( 'admin_menu', array( $this, 'add_authentication_settings_section' ) );
 		add_action( 'admin_init', array( $this, 'register_authentication_settings' ) );
+	}
+
+	function validate_identity() {
+		if ( $this->_access_token ) {
+			$identity_endpoint = add_query_arg( array(
+				'token' => $this->_access_token,
+			), 'https://api.discogs.com/oauth/identity' );
+
+			$identity_response      = wp_remote_get( $identity_endpoint );
+			$identity_response_code = wp_remote_retrieve_response_code( $identity_response );
+
+			if ( $identity_response_code === 200 ) {
+				$identity = json_decode( wp_remote_retrieve_body( $identity_response ) );
+
+				$user_response = wp_remote_get( $identity->resource_url );
+				$this->_discogs_user = json_decode( wp_remote_retrieve_body( $user_response ) );
+			}
+		};
 	}
 
 	public function create_options_page() {
@@ -40,17 +60,29 @@ class OptionsPage {
 	}
 
 	public function render_authentication_notice() {
-		if( ! $this->_access_token ) {
-			$message = sprintf(
-				__('%s <a href="%s" target="_blank">%s</a>.', 'discogs-blocks'),
+		if ( ! $this->_access_token || ! $this->_discogs_user ) {
+			$message          = sprintf(
+				__( '%s <a href="%s" target="_blank">%s</a>.', 'discogs-blocks' ),
 				__( 'You can generate a new access token by visiting', 'discogs-blocks' ),
-				esc_url( 'https://www.discogs.com/settings/developers'),
+				esc_url( 'https://www.discogs.com/settings/developers' ),
 				__( 'Discogs Developer Settings', 'discogs-blocks' )
 			);
+			$notice_class_str = 'notice inline';
 
-			return '<div class="notice inline"><p>' . $message . '</p></div>';
+			if ( $this->_access_token ) {
+				$message          = __( 'Invalid token.', 'discogs-blocks' ) . ' ' . $message;
+				$notice_class_str .= ' error';
+			}
+
+			return '<div class="' . $notice_class_str . '"><p>' . $message . '</p></div>';
 		} else {
-			return '<div class="notice inline notice-info"><p>this will be an identity panel</p></div>';
+			$message            = sprintf(
+				__( 'Validated user <a href="%s">%s</a>.', 'discogs-blocks' ),
+				esc_url( $this->_discogs_user->uri ),
+				esc_html( $this->_discogs_user->name )
+			);
+
+			return '<div class="notice inline notice-info"><p>' . $message . '</p></div>';
 		}
 	}
 
@@ -69,18 +101,20 @@ class OptionsPage {
 
 		add_settings_field(
 			'access_token',
-			'Personal Access Token',
+			__( 'Personal Access Token', 'discogs-blocks' ),
 			array( $this, 'render_token_field' ),
 			$this->_page_slug,
-			$this->_page_slug . '_authentication'
+			$this->_page_slug . '_authentication',
+			array(
+				'label_for' => 'access_token'
+			)
 		);
 	}
 
 	public function render_token_field() {
 		printf(
-			'<label for="%1$s" class="screen-reader-text">%2$s</label><input type="text" id="%1$s" name="%1$s" value="%3$s" />',
+			'<input type="text" id="%1$s" name="%1$s" value="%2$s" />',
 			'access_token',
-			__( 'Enter Personal Access Token', 'discogs-blocks' ),
 			isset( $this->_access_token ) ? esc_attr( $this->_access_token ) : '',
 		);
 	}
@@ -90,7 +124,7 @@ class OptionsPage {
 			$this->_page_slug,
 			'access_token',
 			array(
-				'type' => 'string',
+				'type'              => 'string',
 				'sanitize_callback' => array( $this, 'sanitize_token_field' ),
 				//'show_in_rest' => false,
 				//'label' => '',
