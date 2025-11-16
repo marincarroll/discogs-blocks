c0d102bf57e5d4430947fef23197a6e0488430b9
Marin @Home <marin@marincarroll.com>
2025-11-16 11:30:25 -0500

    moves shared functions to utils file

--- src/blocks/collection/view.js
+++ src/blocks/collection/view.js
@@ -4,14 +4,9 @@ import {
 	getElement,
 	useWatch,
 } from '@wordpress/interactivity';
+import { parseReleaseData, fetchItems, getPageNumbers } from "./utils";
 
 // TODO see if we can convert this file to TS
-// Workaround to use wp-api-fetch and wp-url (not modules) in this script (yes a module).
-// See https://make.wordpress.org/core/2024/03/04/script-modules-in-6-5/#:~:text=There%20is%20a,apiFetch%20%7D%20%3D%20window.wp
-const { apiFetch } = window.wp;
-const { addQueryArgs } = window.wp.url;
-
-const { min, max } = Math;
 
 const { callbacks, actions } = store( 'marincarroll/discogs', {
 	actions: {
@@ -90,65 +85,3 @@ const { callbacks, actions } = store( 'marincarroll/discogs', {
 		},
 	},
 } );
-
-function getPageNumbers( page, pages ) {
-	const ellipsis = '...'; // todo real ellipsis
-	const range = ( lo, hi ) =>
-		Array.from( { length: hi - lo }, ( _, i ) => i + lo );
-	const start = max( 1, min( page - 1, pages - 3 ) );
-	const end = min( pages, max( page + 1, 4 ) );
-	const pageNumbers = [];
-	if ( start > 1 ) {
-		pageNumbers.push( 1 );
-	}
-
-	if ( start > 2 ) {
-		pageNumbers.push( ellipsis );
-	}
-
-	pageNumbers.push( ...range( start, end + 1 ) );
-
-	if ( end < pages - 1 ) {
-		pageNumbers.push( ellipsis );
-	}
-
-	if ( end < pages ) {
-		pageNumbers.push( pages );
-	}
-
-	return pageNumbers;
-}
-
-function parseReleaseData( releases ) {
-	return releases.map( ( release ) => {
-		const {
-			year,
-			title,
-			artists,
-			formats,
-			cover_image: coverImage,
-		} = release.basic_information;
-
-		return {
-			title,
-			year,
-			formats: arrayToString( formats ),
-			artists: arrayToString( artists ),
-			coverImage,
-		};
-	} );
-}
-
-function arrayToString( array ) {
-	const uniqueItems = new Set( array.map( ( item ) => item.name ) );
-	return Array.from( uniqueItems ).join( ', ' );
-}
-
-function fetchItems( perPage, page ) {
-	const path = addQueryArgs( 'marincarroll/v1/discogs/collection', {
-		perPage,
-		page,
-	} );
-
-	return apiFetch( { path } );
-}
