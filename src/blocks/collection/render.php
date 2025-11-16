d637282391c1bde997aa43c5497464e4661c42d3
Marin @Home <marin@marincarroll.com>
2025-11-16 11:47:53 -0500

    moves most logic to template block

--- src/blocks/collection/render.php
+++ src/blocks/collection/render.php
@@ -7,22 +7,12 @@ use WP_Block;
  * @var WP_Block $block;
  */
 
-$placeholder_items = array();
-for( $i = 0; $i < $attributes['perPage']; $i++ ) {
-	$placeholder_items[] = '{}';
-}
-$placeholder_items = join(',', $placeholder_items );
-
 $block_wrapper_attributes = get_block_wrapper_attributes( array(
 	'class' => 'discogs-collection',
-	'data-wp-interactive' => 'marincarroll/discogs',
-	'data-wp-context' => '{"pages":[],"currentPage":1,"perPage":' . $attributes['perPage'] . ',"items":[' . $placeholder_items . ']}',
-	'data-wp-init' => 'actions.init',
-	'data-wp-run' => 'actions.fetchPage',
 ) );
 
 printf(
-	'<div %s>%s<nav data-wp-bind--hidden="!context.maxPages" class="discogs-collection__pagination"><ul data-wp-run="callbacks.buildPaginationButtons"></ul></nav></div>',
+	'<div %s>%s</div>',
 	$block_wrapper_attributes,
 	$content,
 );
--- src/blocks/release-template/render.php
+++ src/blocks/release-template/render.php
@@ -7,6 +7,28 @@ use WP_Block;
  * @var WP_Block $block
  */
 
+$per_page = $block->context["marincarroll-discogs/perPage"];
+$placeholder_items = array();
+for( $i = 0; $i < $per_page; $i++ ) {
+	$placeholder_items[] = '{}';
+}
+$placeholder_items = join(',', $placeholder_items );
+
+$template_wrapper_attributes = array(
+	'class' => 'discogs-release-template',
+   'data-wp-interactive' => 'marincarroll/discogs',
+   'data-wp-context' => '{"pages":[],"currentPage":1,"perPage":' . $per_page . ',"items":[' . $placeholder_items . ']}',
+   'data-wp-init' => 'actions.init',
+   'data-wp-run' => 'actions.fetchPage',
+);
+
+$normalized_attributes = array();
+foreach ( $template_wrapper_attributes as $key => $value ) {
+	$normalized_attributes[] = $key . '="' . esc_attr( $value ) . '"';
+}
+$template_wrapper_attribute_str = implode( ' ', $normalized_attributes );
+
+
 $item_wrapper_attributes = get_block_wrapper_attributes( array(
 	'class' => 'discogs-release',
 	'data-wp-class--placeholder' => '!context.item.title'
@@ -18,7 +40,13 @@ $item = sprintf(
 	$content
 );
 
+$pagination = '<nav data-wp-bind--hidden="!context.maxPages" class="discogs-collection__pagination">
+	<ul data-wp-run="callbacks.buildPaginationButtons"></ul>
+	</nav>';
+
 printf(
-	'<ul class="discogs-release-template"><template data-wp-each="context.items">%s</template></ul>',
+	'<div %s><ul><template data-wp-each="context.items">%s</template></ul>%s</div>',
+	$template_wrapper_attribute_str,
 	$item,
+	$pagination,
 );
