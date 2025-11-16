cd7a7e0699d04f3b888aecc5465c3158434ac9bd
Marin @Home <marin@marincarroll.com>
2025-11-16 12:13:21 -0500

    restructures in anticipation of preview data

--- src/blocks/release-template/edit.js
+++ src/blocks/release-template/edit.js
@@ -3,10 +3,11 @@ import {
 	useInnerBlocksProps,
 	store as blockEditorStore,
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
-	__experimentalUseBlockPreview as useBlockPreview,
+	__experimentalUseBlockPreview as useBlockPreview, BlockContextProvider,
 } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
-import { useState } from '@wordpress/element';
+import { useState, useEffect,useMemo } from '@wordpress/element';
+import {fetchItems, parseReleaseData} from "./utils";
 
 export default function Edit( {
 	clientId,
@@ -15,6 +16,20 @@ export default function Edit( {
 	const blockProps = useBlockProps( {
 		className: 'discogs-release-template',
 	} );
+
+	const [ data, setData ] = useState();
+	useEffect(()=> {
+		fetchItems(perPage, 1).then(response => {
+			const parsedResponse = JSON.parse(response);
+			setData(parsedResponse);
+		});
+	},[perPage]);
+
+	const parsedReleaseData = useMemo(() => {
+		if( data?.releases ) {
+			return parseReleaseData(data.releases);
+		}
+	}, [data])
 
 	const innerBlocksProps = useInnerBlocksProps(
 		{
@@ -39,34 +54,48 @@ export default function Edit( {
 		[ clientId ]
 	);
 
+	const [ selectedIndex, setSelectedIndex ] = useState( 0 );
+
+	return (
+		<div { ...blockProps }>
+			<ul>
+				{ parsedReleaseData?.map( ( item, index ) => {
+					const handleOnClick = () => setSelectedIndex( index );
+
+					return (
+						<BlockContextProvider key={index} value={item}>
+							{  index === selectedIndex
+								 ? <li { ...innerBlocksProps } key="selected" />
+								: <ReleaseBlockPreview
+									blocks={innerBlocksData}
+									handleOnClick={handleOnClick}
+									index={index}
+								/>
+							}
+						</BlockContextProvider>
+					);
+				} ) }
+			</ul>
+		</div>
+	);
+}
+
+function ReleaseBlockPreview({ blocks, handleOnClick, index }) {
 	const blockPreviewProps = useBlockPreview( {
-		blocks: innerBlocksData,
+		blocks,
 		props: {
 			className: 'discogs-release',
 		},
 	} );
 
-	const [ selectedIndex, setSelectedIndex ] = useState( 0 );
-
 	return (
-		<ul { ...blockProps }>
-			{ [ ...Array( perPage ) ].map( ( item, index ) => {
-				if ( index === selectedIndex ) {
-					return <li { ...innerBlocksProps } key="selected" />;
-				}
-				const handleOnClick = () => setSelectedIndex( index );
-
-				return (
-					<li
-						{ ...blockPreviewProps }
-						key={ index }
-						// eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
-						role="button"
-						onClick={ handleOnClick }
-						onKeyDown={ handleOnClick }
-					/>
-				);
-			} ) }
-		</ul>
-	);
+		<li
+			{ ...blockPreviewProps }
+			key={ index }
+			// eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
+			role="button"
+			onClick={ handleOnClick }
+			onKeyDown={ handleOnClick }
+		/>
+	)
 }
